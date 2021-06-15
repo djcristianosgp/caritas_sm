4725f6d525585441b437f54b0838671d0b831ef6
Cristiano <djcristiano.sgp@hotmail.com>
2021-06-15 18:37:30 -0300

    telas iniciais

---
+++ Caritas_SM/Caritas_SM/Views/frmCadPessoa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Caritas_SM.Views
+{
+    public partial class frmCadPessoa : Form
+    {
+        public frmCadPessoa()
+        {
+            InitializeComponent();
+        }
+    }
+}
